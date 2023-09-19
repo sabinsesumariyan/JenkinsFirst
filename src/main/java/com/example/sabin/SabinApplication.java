1054cd9cae374098beda25aeefb84fef19efae9c
Sabin Sesumariyan <75547887+sabinsesumariyan@users.noreply.github.com>
2023-09-19 14:47:09 +0530

    Add files via upload

---
+++ src/main/java/com/example/sabin/SabinApplication.java
@@ -0,0 +1,13 @@
+package com.example.sabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabinApplication.class, args);
+	}
+
+}
